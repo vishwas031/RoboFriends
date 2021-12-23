faa05cfc9086bc4abfb3a2e201ac7c77ce5d20e3
Vishwas Singh <031vishwas@gmail.com>
2021-12-23 22:49:16 +0530

    Updates

---
+++ precache-manifest.bb664f25270e8bfe197f4dd29c14266d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf5e2f251def7ae213af",
+    "url": "/RoboFriends/static/css/main.a45b329a.chunk.css"
+  },
+  {
+    "revision": "cf5e2f251def7ae213af",
+    "url": "/RoboFriends/static/js/main.cf5e2f25.chunk.js"
+  },
+  {
+    "revision": "0bf54a1201dab9a9b136",
+    "url": "/RoboFriends/static/js/1.0bf54a12.chunk.js"
+  },
+  {
+    "revision": "0f3b9a76f6c62332bbc1",
+    "url": "/RoboFriends/static/css/2.c3c08622.chunk.css"
+  },
+  {
+    "revision": "0f3b9a76f6c62332bbc1",
+    "url": "/RoboFriends/static/js/2.0f3b9a76.chunk.js"
+  },
+  {
+    "revision": "2ba4a26884c90d80e190",
+    "url": "/RoboFriends/static/js/runtime~main.2ba4a268.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fd87525831d2aeef072f7302671d88c5",
+    "url": "/RoboFriends/index.html"
+  }
+];
